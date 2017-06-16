c472cb59db1573fbcfa445dc6124fdbff9f8e0b1
brayoh <bkuriakb@gmail.com>
2017-06-16 15:38:31 +0300

    add write to file abilityu

--- madaraka_train.py
+++ madaraka_train.py
@@ -1,14 +1,19 @@
 import json
 from pprint import pprint
 
+with open('db.json') as read_file:
+    data = json.load(read_file)
 
-with open('db.json') as data_file:
-    data = json.load(data_file)
+class BookTrain(object):
+    """docstring for BookTrain."""
+    def __init__(self, data):
+        super(BookTrain, self).__init__()
+        self.data = data
+        
+    def write(self):
+        with open('db.json', 'w') as write_file:
+            json.dump(self.data, write_file)
 
 
-pprint(data['trains'])
-# data_file.write(json.dumps(data))
-
-
-with open('db.json', 'w') as data_file:
-    json.dumps(data)
+if __name__ == "__main__":
+    book_train = BookTrain(data)
