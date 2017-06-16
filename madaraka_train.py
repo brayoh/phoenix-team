f2f21f88684c2278ce2c58d71f3fcfe97bedaa08
MwendeHub <mwendemaryanne@gmail.com>
2017-06-16 16:10:17 +0300

    Add ability to get user details

--- madaraka_train.py
+++ madaraka_train.py
@@ -9,7 +9,20 @@ class BookTrain(object):
     def __init__(self, data):
         super(BookTrain, self).__init__()
         self.data = data
-        
+
+    def user_details():
+		while(1):
+			option = 0
+			print("Select option:")
+			print(1,"Available seats")
+			print(2,"Book seat")
+
+		if (option == 1):
+			name = input("Enter name:")
+			Id = input("Enter ID number:")
+			phone = input("Enter phone number:")
+		else:
+
     def write(self):
         with open('db.json', 'w') as write_file:
             json.dump(self.data, write_file)
---
+++ user_details.py
@@ -0,0 +1,12 @@
+def user_details():
+	while(1):
+		option = 0
+		print("Select option:")
+		print(1,"Available seats")
+		print(2,"Book seat")
+
+	if (option == 1):
+		name = input("Enter name:")
+		id = input("Enter ID number:")
+		phone = input("Enter phone number:")
+	else:
